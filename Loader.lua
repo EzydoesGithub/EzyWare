3889c23847afeb406ab332ed2baab504af5f32f9
EzydoesGithub <81513373+EzydoesGithub@users.noreply.github.com>
2022-07-19 17:41:36 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,13 +7,13 @@
 ]]--
 
 local scriptUrl = ({
-    [10023052234] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl1-2 10023052234
-    [10066152324] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl3 10066152324
-    [10070551211] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl4 10070551211
-    [10088248305] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl5 10088248305
-    [10098309268] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl6 10098309268
-    [10110702274] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl7 10110702274 
-    [10148663400] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/gr.lua", -- lvl8 10148663400
+    [10023052234] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl1-2 10023052234
+    [10066152324] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl3 10066152324
+    [10070551211] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl4 10070551211
+    [10088248305] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl5 10088248305
+    [10098309268] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl6 10098309268
+    [10110702274] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl7 10110702274 
+    [10148663400] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl8 10148663400
 })[game.PlaceId]
 
 if scriptUrl then
